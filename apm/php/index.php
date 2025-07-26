6758984b44e13bd3f4e3f48ddc045a66ab5bdd09
Qanah <i.qanah@gmail.com>
2025-07-26 23:15:15 +0300

    Add Logstash and PHP logging configurations for APM integration

--- apm/php/index.php
+++ apm/php/index.php
@@ -4,6 +4,14 @@ header('Content-Type: application/json');
 // Check if APM extension is loaded
 $apm_loaded = extension_loaded('elastic_apm');
 
+// Generate some sample log entries
+error_log("PHP application accessed at " . date('Y-m-d H:i:s'));
+
+if (isset($_GET['error'])) {
+    error_log("Test error generated for logging demonstration", 0);
+    trigger_error("This is a test error for Logstash collection", E_USER_WARNING);
+}
+
 $response = [
     'message' => 'PHP APM service active',
     'timestamp' => date('Y-m-d H:i:s'),
@@ -11,7 +19,9 @@ $response = [
     'apm_extension_loaded' => $apm_loaded,
     'memory_usage' => memory_get_usage(true),
     'peak_memory' => memory_get_peak_usage(true),
-    'php_version' => PHP_VERSION
+    'php_version' => PHP_VERSION,
+    'logging_enabled' => ini_get('log_errors') ? 'Yes' : 'No',
+    'log_file' => ini_get('error_log')
 ];
 
 if ($apm_loaded) {
